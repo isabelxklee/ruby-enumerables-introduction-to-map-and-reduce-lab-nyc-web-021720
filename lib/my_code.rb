b743f2014b4b959ef7e0ecb27f8269712e9cc7d0
isabel k. lee <hello@isabelklee.co>
2020-02-08 19:48:12 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,88 @@
-# My Code here....
+require 'pry'
+
+# - `map_to_negativize(source_array)`
+# - `map_to_no_change(source_array)`
+# - `map_to_double(source_array)`
+# - `map_to_square(source_array)`
+
+def map_to_negativize(source_array)
+  new_array = []
+  index = 0
+  while index < source_array.length do 
+    new_array << source_array[index] * -1
+    index += 1
+  end 
+  new_array
+end
+
+def map_to_no_change(source_array)
+  new_array = []
+  index = 0 
+  while index < source_array.length do
+    new_array << source_array[index]
+    index += 1
+  end 
+  new_array
+end
+
+def map_to_double(source_array)
+  new_array = []
+  index = 0 
+  while index < source_array.length do 
+    new_array << source_array[index] * 2
+    index += 1
+  end 
+  new_array
+end
+
+def map_to_square(source_array)
+  new_array = [] 
+  index = 0 
+  while index < source_array.length do 
+    new_array << source_array[index] * source_array[index]
+    index += 1
+  end 
+  new_array
+end 
+
+# - `reduce_to_total(source_array, starting_point)`
+# - `reduce_to_all_true(source_array)`
+# - `reduce_to_any_true(source_array)`
+
+def reduce_to_total(source_array, starting_point = 0)
+    total = 0
+    semi_total = 0
+    index = 0 
+      while index < source_array.length do 
+        if starting_point
+          semi_total += source_array[index]
+          total = semi_total + starting_point
+        else
+          total += source_array[index]
+        end
+          index += 1
+      end 
+      return total
+end
+
+def reduce_to_all_true(source_array)
+  index = 0 
+  while index < source_array.length do
+    if source_array[index] == false
+      return false
+    end 
+    index += 1
+  end 
+    return true
+end 
+
+def reduce_to_any_true(source_array)
+  index = 0 
+  while index < source_array.length do 
+    if source_array[index] == true 
+      return true 
+    end 
+    index += 1
+  end 
+    return false 
+end 
