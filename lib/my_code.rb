6e2d23daeb72546a3ed44e0b81aa06715b8f059f
isabel k. lee <hello@isabelklee.co>
2020-02-08 19:30:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -70,7 +70,7 @@ def reduce_to_total(source_array, starting_point)
       index += 1
     end 
   total = semi_total + starting_point
-  total
+  return total
 end
 
 def reduce_to_all_true(source_array)
